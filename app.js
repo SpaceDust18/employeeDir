7a44d6217bab2400a3359c0398d3b3cd233ff367
Claudia <cludama83@gmail.com>
2025-05-08 21:50:05 -0400

    Completed workshop31 employee dir

---
+++ app.js
@@ -0,0 +1,39 @@
+import express from "express"
+import employees from "./db/employees.js"
+
+const app = express()
+
+
+//It helps to write most specific route to avoid errors for the next error test. 
+
+app.route('/employees/random').get((req, res) => {
+    if (!employees || employees.length === 0) {
+        return res.status(404).json({ error: 'Employee not found' });
+    }
+
+    const randomIndex = Math.floor(Math.random() * employees.length);
+    const employee = employees[randomIndex];
+   
+    res.status(200).json(employee);
+});
+
+app.route('/employees/:id').get((req, res) => {
+    const id = parseInt(req.params.id);
+    const employee = employees.find(emp => emp.id === id)
+
+    if (employee) {
+        res.status(200).json(employee)
+    } else {
+        res.status(404).json({ error: 'Employee not found' });
+    }
+});
+
+app.route('/employees').get((req, res) => {
+    res.send(employees)
+})
+
+app.route('/').get((req, res) => {
+    res.send("Hello employees!")
+})
+ 
+export default app
